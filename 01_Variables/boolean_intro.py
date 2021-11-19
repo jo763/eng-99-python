8ffbee72b5c8adfedcd1501b73161d01facdf4a8
jo763 <joe.prollins@otmail.com>
2021-11-19 11:16:06 +0000

    Added boolean and collection files

---
+++ 01_Variables/boolean_intro.py
@@ -0,0 +1,14 @@
+# hi = "hello"
+#
+#
+# print(hi.isalpha())
+# print(hi.islower())
+# print(hi.isupper())
+# print(hi.endswith('o'))
+# print(hi.startswith('h'))
+
+print(bool("ree"))
+print(bool("3+j"))
+print(bool("j"))
+print(bool(True))
+print(bool([3]))
