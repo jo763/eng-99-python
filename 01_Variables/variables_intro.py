f4b3f3b751c79f984fa68ca51419e8dec5d4bc16
jo763 <joe.prollins@otmail.com>
2021-11-19 09:55:38 +0000

    Working with variables and input

---
+++ 01_Variables/variables_intro.py
@@ -0,0 +1,6 @@
+forename = input("What is your forename? ")
+surname = input("What is your surname? ")
+age = input("What is your age? ")
+film_rating = float(input("How would you rate the film Hot Fuzz out of 5? "))
+
+print(f"Your name is {forename} {surname}, you are {age} years old and your opinion of Hot Fuzz {film_rating:.1f} out of 5")
